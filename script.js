8e486c40c0c8834146fdec2aacc2bc552c09c973
Alex Ross <91042341+ItsArcticM@users.noreply.github.com>
2023-07-31 23:36:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const wordList = ["APPLE", "BANAN", "CHURR", "DUVET", "EAGLE", "FLAME", "GRANT", "HYENA", "INANE", "JOKER"];
 
 let selectedWord = "";
-let guessedLetters = new Set();
 let guessesRemaining = 6;
 
 function pickRandomWord() {
@@ -9,8 +8,7 @@ function pickRandomWord() {
 }
 
 function initializeGame() {
-  selectedWord = pickRandomWord();
-  guessedLetters = new Set();
+  selectedWord = pickRandomWord().toUpperCase();
   guessesRemaining = 6;
   updateWordDisplay();
   document.getElementById("guessInput").value = "";
@@ -19,9 +17,9 @@ function initializeGame() {
 
 function updateWordDisplay() {
   const wordDisplayElement = document.getElementById("wordDisplay");
-  wordDisplayElement.textContent = selectedWord
+  wordDisplayElement.innerHTML = selectedWord
     .split('')
-    .map((letter) => guessedLetters.has(letter) ? letter : "•")
+    .map((letter) => `<div class="letter">${guessedLetters.has(letter) ? letter : "•"}</div>`)
     .join('');
 }
 
@@ -36,11 +34,25 @@ function guessWord() {
   }
 
   guessesRemaining--;
-  guessedLetters = new Set(guessedWord); // Update guessedLetters with the new guess
+  const guessedLetters = new Set(guessedWord);
+
+  let correctLettersCount = 0;
+  for (const letter of selectedWord) {
+    if (guessedLetters.has(letter)) {
+      correctLettersCount++;
+      guessedLetters.delete(letter);
+    }
+  }
+
   updateWordDisplay();
 
-  // The rest of the code remains the same as in the previous responses
-  // ...
+  if (correctLettersCount === 5) {
+    showMessage("Congratulations! You've guessed the word!");
+  } else if (guessesRemaining === 0) {
+    showMessage(`Game over! The word was "${selectedWord}".`);
+  } else {
+    showMessage(`Correct letters: ${correctLettersCount}, Guesses remaining: ${guessesRemaining}`);
+  }
 }
 
 function resetGame() {
