3634fd2cc2da7a4a49059438f3fc3a7b056342e8
Alex Ross <91042341+ItsArcticM@users.noreply.github.com>
2023-07-31 23:39:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,69 @@
-const wordList = ["APPLE", "BANAN", "CHURR", "DUVET", "EAGLE", "FLAME", "GRANT", "HYENA", "INANE", "JOKER"];
+<script>
+  const wordList = ["APPLE", "BANAN", "CHURR", "DUVET", "EAGLE", "FLAME", "GRANT", "HYENA", "INANE", "JOKER"];
 
-let selectedWord = "";
-let guessesRemaining = 6;
+  let selectedWord = "";
+  let guessedLetters = new Set();
+  let guessesRemaining = 6;
 
-function pickRandomWord() {
-  return wordList[Math.floor(Math.random() * wordList.length)];
-}
-
-function initializeGame() {
-  selectedWord = pickRandomWord().toUpperCase();
-  guessesRemaining = 6;
-  updateWordDisplay();
-  document.getElementById("guessInput").value = "";
-  document.getElementById("message").textContent = "";
-}
-
-function updateWordDisplay() {
-  const wordDisplayElement = document.getElementById("wordDisplay");
-  wordDisplayElement.innerHTML = selectedWord
-    .split('')
-    .map((letter) => `<div class="letter">${guessedLetters.has(letter) ? letter : "•"}</div>`)
-    .join('');
-}
-
-function guessWord() {
-  const guessInput = document.getElementById("guessInput");
-  const guessedWord = guessInput.value.toUpperCase();
-  guessInput.value = "";
-
-  if (guessedWord.length !== 5 || !/^[A-Z]+$/.test(guessedWord)) {
-    showMessage("Please enter a valid 5-letter word.");
-    return;
+  function pickRandomWord() {
+    return wordList[Math.floor(Math.random() * wordList.length)];
   }
 
-  guessesRemaining--;
-  const guessedLetters = new Set(guessedWord);
+  function initializeGame() {
+    selectedWord = pickRandomWord();
+    guessedLetters = new Set();
+    guessesRemaining = 6;
+    updateWordDisplay();
+    document.getElementById("message").textContent = "";
+  }
 
-  let correctLettersCount = 0;
-  for (const letter of selectedWord) {
-    if (guessedLetters.has(letter)) {
-      correctLettersCount++;
-      guessedLetters.delete(letter);
+  function updateWordDisplay() {
+    const wordDisplayElement = document.getElementById("wordDisplay");
+    wordDisplayElement.textContent = selectedWord
+      .split('')
+      .map((letter) => guessedLetters.has(letter) ? letter : "•")
+      .join('');
+  }
+
+  function guessWord() {
+    const guessInput = document.getElementById("guessInput");
+    const guessedWord = guessInput.value.toUpperCase();
+    guessInput.value = "";
+
+    if (guessedWord.length !== 5 || !/^[A-Z]+$/.test(guessedWord)) {
+      showMessage("Please enter a valid 5-letter word.");
+      return;
+    }
+
+    guessesRemaining--;
+    guessedLetters = new Set(guessedWord);
+
+    let correctLettersCount = 0;
+    for (const letter of selectedWord) {
+      if (guessedLetters.has(letter)) {
+        correctLettersCount++;
+        guessedLetters.delete(letter);
+      }
+    }
+
+    updateWordDisplay();
+
+    if (correctLettersCount === 5) {
+      showMessage("Congratulations! You've guessed the word!");
+    } else if (guessesRemaining === 0) {
+      showMessage(`Game over! The word was "${selectedWord}".`);
+    } else {
+      showMessage(`Correct letters: ${correctLettersCount}, Guesses remaining: ${guessesRemaining}`);
     }
   }
 
-  updateWordDisplay();
-
-  if (correctLettersCount === 5) {
-    showMessage("Congratulations! You've guessed the word!");
-  } else if (guessesRemaining === 0) {
-    showMessage(`Game over! The word was "${selectedWord}".`);
-  } else {
-    showMessage(`Correct letters: ${correctLettersCount}, Guesses remaining: ${guessesRemaining}`);
+  function resetGame() {
+    initializeGame();
   }
-}
 
-function resetGame() {
+  function showMessage(message) {
+    document.getElementById("message").textContent = message;
+  }
+
   initializeGame();
-}
-
-function showMessage(message) {
-  document.getElementById("message").textContent = message;
-}
-
-initializeGame();
+</script>
